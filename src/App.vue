49e23b758032c2be321024c1d0d173e6d6922194
MaxBlach <maxime.blachere@livecampus.tech>
2023-06-13 10:55:53 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+  margin: 0;
+  padding: 0;
+  font-weight: normal;
+}
+
+ol, ul {
+  list-style: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1>Main</h1>
+  </main>
+</template>
